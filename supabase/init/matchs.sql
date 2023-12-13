7f5709687a6741bd44d004bc098e1cc4a5d8e28b
frub <benjamin.francheteau@cnieg.fr>
2023-12-13 15:41:54 +0100

    DEBUT du projet

--- supabase/init/matchs.sql
+++ supabase/init/matchs.sql
@@ -8,7 +8,7 @@ create table "public"."matchs" (
     "venue" character varying,
     "api_id" bigint,
     "city" character varying,
-    "round" text varying,
+    "round" text null,
     "status" text null,
     "team_a_goal" smallint null,
     "team_b_goal" smallint null,
