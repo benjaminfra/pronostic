57a5caff51253a9643cbc748b980f83c053fe122
frub <benjamin.francheteau@cnieg.fr>
2024-01-10 17:21:46 +0100

    style

--- src/components/menu/MenuItem.tsx
+++ src/components/menu/MenuItem.tsx
@@ -10,7 +10,7 @@ const MenuItem: React.FC<MenuItemProps> = ({ name, icon, link }) => {
   return (
     <Link
       to={link}
-      className="flex flex-col md:flex-row items-center px-4 py-2 my-10 ease-in-out hover:-translate-y-1 hover:scale-110 transition-all"
+      className="flex flex-col md:flex-row items-center px-4 py-2 md:my-10 my-2 ease-in-out hover:-translate-y-1 hover:scale-110 transition-all"
     >
       <div className="md:mr-3">{icon}</div>
       <div className="2xl:text-3xl xl:text-2xl font-medium">{name}</div>
