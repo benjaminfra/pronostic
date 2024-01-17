0c9c9b73a2f15471b72d31aa9f39d4a698141fcb
frub <benjamin.francheteau@cnieg.fr>
2024-01-17 11:06:23 +0100

    WIP

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,12 +1,11 @@
 type CardProps = {
-  padding?: number;
   children: React.ReactNode;
 };
 
-const Card: React.FC<CardProps> = ({ padding = 20, children }) => {
+const Card: React.FC<CardProps> = ({ children }) => {
   const animationStyle =
     "transition ease-in-out duration-300 hover:-translate-y-1 hover:scale-110";
-  const cardStyle = `bg-yellow-gradient border rounded-xl p-${padding} ${animationStyle}`;
+  const cardStyle = `bg-yellow-gradient border rounded-xl p-20 ${animationStyle}`;
   return (
     <div className={cardStyle}>
       <div className="text-center text-black">{children}</div>
